b598c64c4c5ce3ed62070c43a98edea5898019ee
Jersonrn <jersonrn@gmail.com>
2024-03-18 03:21:47 -0500

    Sigmoid activation function added

---
+++ godot/scripts/sigmoid.gd
@@ -0,0 +1,34 @@
+class_name  Sigmoid
+
+
+var inputs : PackedFloat32Array
+
+func _init():
+	pass
+
+
+func _ready():
+	pass
+
+
+func _to_string():
+	return "Sigmoid()".format({})
+
+
+func forward(xx: PackedFloat32Array) -> PackedFloat32Array:
+	self.inputs = xx
+	var output: PackedFloat32Array = []
+
+	for x  in xx: output.append( 1 / (1 + exp(-x)) )
+
+	return output
+
+
+func calculate_derivative() -> Tensor:
+	var output := Tensor.new()
+
+	for x in self.inputs:
+		output.append(exp(-x) * (1 + exp(-x)) ** -2)
+
+	return  output
+
