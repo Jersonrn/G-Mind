b41fb35f9f7d820d99564c163c4ba4fb422bf75f
Jersonrn <jersonrn@gmail.com>
2024-03-28 15:23:48 -0500

    alpha parameter renamed to negative_slope in LeakyRelu class

--- godot/scripts/leaky_relu.gd
+++ godot/scripts/leaky_relu.gd
@@ -1,13 +1,13 @@
 class_name LeakyRelu
 
 
-@export var alpha: float = 0.01
+@export var negative_slope: float = 0.01
 
 var inputs : PackedFloat32Array
 
 
-func _init(alpha_: float = 0.01):
-	self.alpha = alpha_
+func _init(negative_slope_: float = 0.01):
+	self.negative_slope = negative_slope_
 
 
 func _ready():
@@ -15,14 +15,14 @@ func _ready():
 
 
 func _to_string():
-	return "LeakyRelu(alpha={alpha})".format({"alpha":self.alpha})
+	return "LeakyRelu(negative_slope={negative_slope})".format({"negative_slope":self.negative_slope})
 
 
 func forward(xx: PackedFloat32Array) -> PackedFloat32Array:
 	self.inputs = xx
 	var output: PackedFloat32Array = []
 
-	for x in xx: output.append(max(self.alpha * x, x))
+	for x in xx: output.append(max(self.negative_slope * x, x))
 
 	return output
 
@@ -34,7 +34,7 @@ func calculate_derivative() -> Tensor:
 		if x >= 0:
 			output.append(1.0)
 		else:
-			output.append(self.alpha)
+			output.append(self.negative_slope)
 
 	return output
 
