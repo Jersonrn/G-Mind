1837e47e1b1e9eeed20fd7273972a323426e7447
Jersonrn <jersonrn@gmail.com>
2024-06-22 15:07:04 -0500

    Added class 'Utils' and 'is_close' function

--- godot/scripts/utils.gd
+++ godot/scripts/utils.gd
@@ -1,4 +1,5 @@
-extends Node
+class_name Utils
+extends RefCounted
 
 func linspace(start: float = -1, stop: float = 1, num: int = 100) -> Array:
 	var res: Array = []
@@ -6,3 +7,12 @@ func linspace(start: float = -1, stop: float = 1, num: int = 100) -> Array:
 	for i in range(num): res.append(start + i * step)
 
 	return res
+
+func is_close(a: float, b: float, rtol: float, atol: float):
+	if a == b: return true
+
+	var diff = abs(a - b)
+	if diff <= atol: return true
+
+	if diff <= rtol * max(abs(a), abs(b)): return true
+	else: return false
